7888b23cee5af84c6cf1535a6bd0953ffa3a60df
Hasan <mhasanravda@gmail.com>
2025-08-09 22:16:27 +0530

    Initial Commit - Working v1

---
+++ Email-Writer-Backend/src/main/java/com/email/writer/products/Product.java
@@ -0,0 +1,19 @@
+package com.email.writer.products;
+
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+import lombok.ToString;
+
+@Data
+@NoArgsConstructor
+@AllArgsConstructor
+@ToString
+public class Product {
+    int id;
+    String name;
+    double price;
+
+
+
+}
---
+++ Email-Writer-Extension/content.js
@@ -0,0 +1,138 @@
+console.log("Email Writer Extension - Content Script Loaded");
+
+function createAIButton() {
+    const button = document.createElement('div');
+    button.className = 'T-I J-J5-Ji aoO v7 T-I-atl L3 dC btA';
+    button.style.marginRight = '8px';
+
+    button.innerHTML = 'AI reply';
+    button.setAttribute('role', 'button');
+    button.setAttribute('data-tooltip', 'Generate AI Reply');
+    return button;
+}
+
+function getEmailContent() {
+    // a3s ail
+    const selectors = [
+        '.h7',
+        '.a3s.aiL',
+        '.gmail_quote',
+        '[role="presentation"]',
+    ];
+
+    for (const selector of selectors) {
+        const content = document.querySelector(selector);
+        if (content) {
+            console.log(`Found email content using selector: ${selector}`);
+            return content.innerText.trim();
+        }
+    }
+    return null;
+
+}
+
+function findComposeToolbar() {
+    const selectors = [
+        '.btC',
+        '.aDh',
+        '[role="dialog"]',
+        '.gU.Up',
+    ]
+    for (const selector of selectors) {
+        const toolbar = document.querySelector(selector);
+        if (toolbar) {
+            console.log(`Found compose toolbar using selector: ${selector}`);
+            return toolbar;
+        }
+    }
+    return null;
+}
+
+
+// Will inject a button next to reply in Gmail compose window
+function injectButton() {
+    const existingButton = document.querySelector('.ai-reply-button');
+    if (existingButton) existingButton.remove();
+
+    const toolbar = findComposeToolbar();
+    if (!toolbar) {
+        console.log('Toolbar not found');
+        return;
+    }
+    console.log('Toolbar not found! Creating AI button');
+    const button = createAIButton();
+    button.classList.add('ai-reply-button');
+    button.textContent = 'AI Reply';
+
+
+    // Will be having all the logic to handle click events
+    button.addEventListener('click', async () => {
+        try {
+            button.innerHTML = 'Generating...';
+            button.disabled = true;
+
+            const emailContent = getEmailContent();
+            // TODO : need to change the api after deploying
+            const response = await fetch('http://localhost:8081/api/email/generate', {
+                method: 'POST',
+                headers: {
+                    'Content-Type': 'application/json',
+                },
+                body: JSON.stringify({
+                    emailContent: emailContent,
+                    tone: 'professional', // You can change this to any tone you want 
+                })
+            });
+
+            if (!response.ok) {
+                throw new Error('API Request Failed');
+            }
+            const generatedReply = await response.text();
+            const composeBox = document.querySelector('[role="textbox"][g_editable="true"]');
+
+            if (composeBox) {
+                composeBox.focus();
+                // document.execCommand('insertText', false,generatedReply);
+                composeBox.innerHTML += generatedReply;
+
+            } else {
+                console.error('Compose Box was not found');
+            }
+        } catch (error) {
+            console.error(error);
+            alert('Error generating AI reply:');
+        } finally {
+            button.innerHTML = 'AI reply';
+            button.disabled = false;
+        }
+    });
+
+    toolbar.insertBefore(button, toolbar.firstChild);
+    console.log("AI Reply Button Injected");
+}
+
+const observer = new MutationObserver((mutations) => {
+    for (const mutation of mutations) {
+        const addedNodes = Array.from(mutation.addedNodes);
+        const hasComposeElements = addedNodes.some(node => {
+            if (node instanceof Element) {
+                return node.matches('.aDh,.btC,[role="dialog"]') ||
+                    node.querySelector('.aDh,.btC,[role="dialog"]');
+            }
+            return false;
+        }
+
+        );
+
+        if (hasComposeElements) {
+            console.log("Compose Window Detected");
+            setTimeout(injectButton, 500);
+        }
+
+    }
+});
+
+observer.observe(document.body, {
+    childList: true,
+    subtree: true
+});
